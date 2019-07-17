e87cf5f941af04881986b860089ac93be9ed7859
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-17 18:21:23 +0100

    Created post helper

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,7 +10,13 @@ function findById (id) {
     return db('schemes').where({ id }).first()
 }
 
+function add(scheme){
+    return db('schemes').insert(scheme)  .then(ids => ({ id: ids[0] }));
+
+}
+
 module.exports = {
     find,
-    findById
+    findById,
+    add
 }
