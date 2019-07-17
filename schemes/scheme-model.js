ea8d83893f06ab6860dae957ea33a1bae3aed763
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-17 18:12:07 +0100

    Completed GET by ID helper function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -6,7 +6,11 @@ function find () {
     return db('schemes')
 }
 
+function findById (id) {
+    return db('schemes').where({ id }).first()
+}
 
 module.exports = {
-    find
+    find,
+    findById
 }
