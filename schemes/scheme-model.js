aab3756ed49fd7975bdb8718ac4ccb21f07ce3e2
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-17 19:06:56 +0100

    Built update and remove helper functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,13 +10,40 @@ function findById (id) {
     return db('schemes').where({ id }).first()
 }
 
+function findSteps(id) {
+    return db('steps')
+    .join('schemes', 'steps.scheme_id', 'schemes.id')
+    .select('step_number', 'instructions')
+    .where('scheme_id', schemeId)
+    .orderBy('steps.step_number');
+}
+
 function add(scheme){
-    return db('schemes').insert(scheme)  .then(ids => ({ id: ids[0] }));
+    return db('schemes').insert(scheme) .then(ids => {
+        return findById(ids[0]);
+     });
 
 }
+function update(changes, id) {
+    return db('schemes')
+       .where({ id })
+       .update(changes)
+       .then(() => {
+          return findById(id)
+       });
+ }
+ 
+ function remove(id) {
+    return db('schemes')
+       .where('id', id)
+       .del()
+ } 
 
 module.exports = {
     find,
     findById,
-    add
+    add,
+    findSteps,
+    remove,
+    update
 }
